550ecc8982f4326cb2e6d45b98a335faedc11be6
Diwas <diwas.bhattaraiii@gmail.com>
2023-04-19 16:13:30 -0600

    log escape changes

--- app.py
+++ app.py
@@ -36,10 +36,9 @@ def home():
 def save_to_log(request_ip, request_time, progression, success, error, response, openai_response={}):
     # with open('./_logs/log.txt', 'a') as f:
         # f.write('\n'+str(request_time) + '|' + request_ip + '|' + progression + '|' + str(success) + '|' + re.sub(r"[\n\t]*", "", error) + '|' + str((datetime.now()-request_time).seconds) + '|' + re.sub(r"[\n\t]*", "", str(response)))        
-    print((str(request_time) + '|' + request_ip + '|' + progression + '|' + str(success) + '|' + re.sub(r"[\n\t].?", " ", error) + '|' + str((datetime.now()-request_time).seconds) + '|' + re.sub(r"[\n\t].?", " ", str(response)), re.sub(r"[\n\t].?", "", str(openai_response))))
+    print((str(request_time) + '|' + request_ip + '|' + progression + '|' + str(success) + '|' + re.sub(r"[\n\t]", " ", error) + '|' + str((datetime.now()-request_time).seconds) + '|' + re.sub(r"[\n\t]", " ", str(response)), re.sub(r"[\n\t]", "", str(openai_response))))
 
 @app.route('/api/reharmonize', methods = ['GET', 'OPTIONS'])
-@cross_origin()
 def disp():
     progression = request.args.get('progression')
     # print(progression)
