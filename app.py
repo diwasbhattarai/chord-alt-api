bab7ccd92ded4299be514c430dbe7c1ac65e9e17
Diwas <diwas.bhattaraiii@gmail.com>
2023-04-19 15:47:47 -0600

    log changes

--- app.py
+++ app.py
@@ -36,7 +36,7 @@ def home():
 def save_to_log(request_ip, request_time, progression, success, error, response):
     # with open('./_logs/log.txt', 'a') as f:
         # f.write('\n'+str(request_time) + '|' + request_ip + '|' + progression + '|' + str(success) + '|' + re.sub(r"[\n\t]*", "", error) + '|' + str((datetime.now()-request_time).seconds) + '|' + re.sub(r"[\n\t]*", "", str(response)))        
-    print(('\n'+str(request_time) + '|' + request_ip + '|' + progression + '|' + str(success) + '|' + re.sub(r"[\n\t]?", " ", error) + '|' + str((datetime.now()-request_time).seconds) + '|' + re.sub(r"[\n\t]?", " ", str(response))))
+    print(('\n'+str(request_time) + '|' + request_ip + '|' + progression + '|' + str(success) + '|' + re.sub(r"[\n\t].?", " ", error) + '|' + str((datetime.now()-request_time).seconds) + '|' + re.sub(r"[\n\t].?", " ", str(response))))
 
 @app.route('/api/reharmonize', methods = ['GET', 'OPTIONS'])
 @cross_origin()
