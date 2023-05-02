51a36b8db803bdab5230647918e220000b482e60
Diwas <diwas.bhattaraiii@gmail.com>
2023-05-02 14:10:59 -0600

    configured startup script for azure

--- startup.sh
+++ startup.sh
@@ -1,9 +1,6 @@
 \#!/bin/bash
-apt-get -y install ffmpeg
-apt-get -y install supervisor
-mkdir /var/log/celery/
-cp supervisord.conf /etc/supervisor/conf.d/
-supervisorctl reread
-supervisorctl update
-supervisord
-gunicorn  -w 1 --threads 12 --bind '0.0.0.0:8000' app:server --timeout 30
+apt-get -y install redis-server
+pip install -r requirements.txt
+gnome-terminal --tab --title="Redis" --command="bash -c 'redis-server'"
+gnome-terminal --tab --title="Celery" --command="bash -c 'celery -A app.celery worker --loglevel=info'"
+gunicorn --bind=0.0.0.0 --workers=4  app:app --timeout=600
